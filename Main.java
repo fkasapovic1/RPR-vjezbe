47b084c5cee0108deb12fcc2f10e233fc62d6033
fkasapovic1 <fkasapovic1@etf.unsa.ba>
2023-11-02 00:58:18 +0100

    Prvi zadatak

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main{
+
+public static void main(String[] Args){
+Scanner ulaz=new Scanner(System.in);
+int x,y;
+System.out.println("Unesi prvi broj: ");
+x=ulaz.nextInt();
+System.out.println("Unesi drugi broj: ");
+y=ulaz.nextInt();
+}
+}
